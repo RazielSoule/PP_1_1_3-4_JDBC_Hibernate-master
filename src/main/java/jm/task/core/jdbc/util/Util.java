31836c075a4aec3d1dfe687aebe960851b9f9aab
RazielSoule <soulenore@mail.ru>
2024-10-12 04:18:59 +0500

    Revert "Fixes to 1.1.3"
    This reverts commit 04319d7c2ea8b90287572ce63ab311b2df0a2a3b.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,17 +1,75 @@
 package jm.task.core.jdbc.util;
 
+<<<<<<< Updated upstream
+=======
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+>>>>>>> Stashed changes
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+<<<<<<< Updated upstream
 public class Util {
+=======
+public class Util{
+    private static final Logger logger = LoggerFactory.getLogger(Util.class);
+
+>>>>>>> Stashed changes
     static final String DB_DRIVER = "org.postgresql.Driver";
     static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/postgres";
     static final String USER = "postgres";
     static final String PASS = "1243";
 
     public Connection getConnection() throws ClassNotFoundException, SQLException {
+        logger.debug("Connecting to DB using JDBC");
+
         Class.forName(DB_DRIVER);
         return DriverManager.getConnection(DB_URL, USER, PASS);
     }
+<<<<<<< Updated upstream
+=======
+
+    //Hibernate
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            logger.debug("Connecting to DB using Hibernate");
+
+            Configuration configuration = new Configuration();
+
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, DB_DRIVER);
+            settings.put(Environment.URL, DB_URL);
+            settings.put(Environment.USER, USER);
+            settings.put(Environment.PASS, PASS);
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+
+            settings.put(Environment.SHOW_SQL, "true");
+
+            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+            settings.put(Environment.HBM2DDL_AUTO, "");
+
+            configuration.setProperties(settings);
+
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        }
+        return sessionFactory;
+    }
+>>>>>>> Stashed changes
 }
